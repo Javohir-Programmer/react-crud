89fbfe5529d6fce0f4993b26ef3111ddd5dad0b8
Javohir <jabduraimov2002@gmail.com>
2024-06-19 14:29:32 +0500

    nmadr

--- src/pages/car.jsx
+++ src/pages/car.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { db } from "../firebase";
-import { collection, addDoc, deleteDoc, doc, getDocs, updateDoc } from "firebase/firestore";
+import { collection, addDoc, deleteDoc, doc, getDocs, updateDoc, onSnapshot } from "firebase/firestore";
 import { v4 as uuidv4 } from 'uuid';
 
 const Car = () => {
@@ -11,16 +11,29 @@ const Car = () => {
     const [id, setId] = useState('');
     const [show, setShow] = useState(true);
 
+    let data = collection(db, 'box');
+
     useEffect(() => {
-        const getData = async () => {
-            const dataBase = await getDocs(collection(db, 'box'));
-            const itemsList = dataBase.docs.map(doc => ({
-                ...doc.data(), id: doc.id,
-            }));
-            setCar(itemsList);
-            console.log(itemsList);
+        // const getData = async () => {
+        //     const dataBase = await getDocs(collection(db, 'box'));
+        //     const itemsList = dataBase.docs.map(doc => ({
+        //         ...doc.data(), id: doc.id,
+        //     }));
+        //     setCar(itemsList);
+        //     console.log(itemsList);
+        // }
+        // getData();
+
+        onSnapshot(
+        data, (snapshot) => {
+            let malumot = [];
+
+            snapshot.docs.forEach((doc) => {
+                malumot.push({...doc.data(), id: doc.id });
+            })
+            setCar(malumot);
         }
-        getData();
+        )
     }, []);
 
     const handleCreate = async (e) => {
@@ -32,14 +45,14 @@ const Car = () => {
             img: img,
             id: uuidv4()
         });
-        window.location.reload();
+        // window.location.reload();
     }
 
     const handleDelete = async (id) => {
         const deletePost = doc(db, 'box', id);
         await deleteDoc(deletePost);
-        setCar(car.filter(item => item.id !== id)); 
-        window.location.reload();
+        setCar(car.filter(item => item.id !== id));
+        // window.location.reload();
     };
 
     const handleEdit = async (name, job, img, id) => {
@@ -59,12 +72,12 @@ const Car = () => {
         console.log("Updating item with ID:", id); // Debugging log
         const updateData = doc(db, 'box', id);
         await updateDoc(updateData, { name, job, img });
-        window.location.reload();
+        // window.location.reload();
     }
 
     return (
-        <div>
-            <div className="flex flex-wrap w-full mb-20">
+        <div className='' id='nmadr'>
+            <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2">
                 <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900"></h1>
                     {/* <div className="h-1 w-20 bg-indigo-500 rounded"></div> */}
@@ -105,7 +118,7 @@ const Car = () => {
                                             <h2 className="leading-relaxed text-center text-[25px] text-base">Kasbi: {item.job}</h2>
                                             <br />
                                             <button className="border rounded-[16px]  px-4 py-2 mt-2 ml-[100px] bg-red-800 text-white" onClick={() => handleDelete(item.id)}>Delete</button>
-                                            <button className="border rounded-[16px] px-4 py-2 mt-2 bg-blue-500 text-white" onClick={() => handleEdit(item.name, item.job, item.img, item.id)}>Update</button>
+                                            <button className="border rounded-[16px] px-4 py-2 mt-2 bg-blue-500 text-white" onClick={() => handleEdit(item.name, item.job, item.img, item.id)}><a href='#nmadr'>Update</a></button>
                                         </div>
                                     </div>
                                 </div>
